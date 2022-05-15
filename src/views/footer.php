29eadf31200b7a654cda2b03a6739194f784fb1b
Richard Walsh <richwalsh2@googlemail.com>
2022-05-15 18:24:08 +0100

    style iplayer card

--- src/views/footer.php
+++ src/views/footer.php
@@ -1,40 +1,62 @@
 <footer class="footer">
-    <div class="footer-heading">
-        <h2>Best of the BBC</h2>
-    </div>
-    <div class="iplayer-container">
-        <div class="iplayer__card"></div>
-        <div class="iplayer__card"></div>
-        <div class="iplayer__card"></div>
-        <div class="iplayer__card"></div>
-    </div>
-    <div class="footer-nav-container">
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-    </div>
-    <div class="legal-nav-container">
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-    </div>
-    <div class="copyright-container">
-        <p><span data-boldfont>Copyright © 2022 BBC.</span>&nbsp;The BBC is not responsible for the content of external sites.&nbsp;<a>Read about our approach to external linking.</a> </p>
+    <div class="footer-container">
+        <div class="footer-heading">
+            <h2>Best of the BBC</h2>
+        </div>
+        <div class="iplayer-container">
+            <div class="iplayer__card">
+                <img class="iplayer__card--img" src="../assets/iplayer_img.jpg">
+                <button class="iplayer__card--button"><ion-icon name="play-circle"></ion-icon></button>
+                <p class="iplayer__card--title">Lorem ipsum dolor sit amet</p>
+                <p class="iplayer__card--sub-title">BBC iPlayer</p>
+            </div>
+            <div class="iplayer__card">
+                <img class="iplayer__card--img" src="../assets/iplayer_img.jpg">
+                <button class="iplayer__card--button"><ion-icon name="play-circle"></ion-icon></button>
+                <p class="iplayer__card--title">Lorem ipsum dolor sit amet</p>
+                <p class="iplayer__card--sub-title">BBC iPlayer</p>
+            </div>
+            <div class="iplayer__card">
+                <img class="iplayer__card--img" src="../assets/iplayer_img.jpg">
+                <button class="iplayer__card--button"><ion-icon name="play-circle"></ion-icon></button>
+                <p class="iplayer__card--title">Lorem ipsum dolor sit amet</p>
+                <p class="iplayer__card--sub-title">BBC iPlayer</p>
+            </div>
+            <div class="iplayer__card">
+                <img class="iplayer__card--img" src="../assets/iplayer_img.jpg">
+                <button class="iplayer__card--button"><ion-icon name="play-circle"></ion-icon></button>
+                <p class="iplayer__card--title">Lorem ipsum dolor sit amet</p>
+                <p class="iplayer__card--sub-title">BBC iPlayer</p>
+            </div>
+        </div>
+        <div class="footer-nav-container">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div class="legal-nav-container">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div class="copyright-container">
+            <p><span data-boldfont>Copyright © 2022 BBC.</span>&nbsp;The BBC is not responsible for the content of external sites.&nbsp;<a>Read about our approach to external linking.</a> </p>
+        </div>
     </div>
 </footer>
