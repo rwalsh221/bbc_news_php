9db84d69c364f66f0ede6c66b328b9248141ab74
Richard Walsh <richwalsh2@googlemail.com>
2022-05-15 16:08:12 +0100

    continue header

--- src/views/header.php
+++ src/views/header.php
@@ -43,12 +43,41 @@
                     <li>
                         <a>Bitesize</a>
                     </li>
+                    <li>
+                        <a>
+                            <ion-icon name="ellipsis-horizontal"></ion-icon>
+                        </a>
+                    </li>
                 </ul>
             </div>
-            <div class="nav_search">
-
+            <div class="nav__search">
+                <button>
+                    <span><ion-icon name="search"></ion-icon>&nbsp;	&nbsp;Search BBC</span>
+                </button>
             </div>
         </nav>
     </div>
-    
+    <div class="heading__primary">
+        <h1>NEWS</h1>
+    </div>
+    <div class="news-nav-container">
+        <nav class="news__nav">
+            <ul>
+                <li><span>home</span></li>
+                <li>war in ukraine</li>
+                <li>coronovirus</li>
+                <li>climate</li>
+                <li>uk</li>
+                <li>world</li>
+                <li>business</li>
+                <li>politics</li>
+                <li>tech</li>
+                <li>science</li>
+                <li>health</li>
+                <li>family & education</li>
+                <li>entertainment & arts</li>
+                <li><span>more&nbsp;</span><ion-icon name="caret-down"></ion-icon></li>
+            </ul>
+        </nav>
+    </div>
 </header>
