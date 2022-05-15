fdcda0d95c11e8fb5d08ff5387f3f73347bc5932
Richard Walsh <richwalsh2@googlemail.com>
2022-05-15 17:42:33 +0100

    start footer

--- src/views/footer.php
+++ src/views/footer.php
@@ -1,3 +1,40 @@
-<footer>
-    
+<footer class="footer">
+    <div class="footer-heading">
+        <h2>Best of the BBC</h2>
+    </div>
+    <div class="iplayer-container">
+        <div class="iplayer__card"></div>
+        <div class="iplayer__card"></div>
+        <div class="iplayer__card"></div>
+        <div class="iplayer__card"></div>
+    </div>
+    <div class="footer-nav-container">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+    <div class="legal-nav-container">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+    <div class="copyright-container">
+        <p><span data-boldfont>Copyright © 2022 BBC.</span>&nbsp;The BBC is not responsible for the content of external sites.&nbsp;<a>Read about our approach to external linking.</a> </p>
+    </div>
 </footer>
